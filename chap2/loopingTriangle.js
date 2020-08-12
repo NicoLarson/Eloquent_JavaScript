b6c1d5ab1d972c4dff25e8c6e75ceaba2910f69c
Nicolas Yang <nicolas.yang.1@gmail.com>
2020-08-12 14:46:35 +0200

    Exo chap2 done

---
+++ chap2/loopingTriangle.js
@@ -0,0 +1,10 @@
+let hash = "#"
+for (let i = 0; i < 7; i++) {
+    let addHash = "#"
+    console.log(hash)
+    hash += addHash
+}
+
+for (let line = "#"; line.length<8; line+="#"){
+    console.log(line)
+} 

83945ff150ff7a5d52d9c8dbda666d5d6820b926
Nicolas Yang <nicolas.yang.1@gmail.com>
2020-08-21 14:38:57 +0200

    erreur exercice reversArrayInPlace

--- chap4/reversing_an_array.js
+++ chap4/reversing_an_array.js
@@ -1,10 +1,25 @@
 let reverseArray = (arr) => {
-
-
-    return arrReverse
+	let arrReverse = []
+	for (let i = arr.length - 1; i > -1; i--) {
+		arrReverse.push(arr[i])
+	}
+	return arrReverse
 }
 
-let reverseArrayInPlace = () => {
-
-    
+let reverseArrayInPlace = (arr) => {
+	let arrReverse = []
+	for (let i = arr.length - 1; i > -1; i--) {
+		arrReverse.push(arr[i])
+	}
+	arr = arrReverse
+	return arr
 }
+
+
+console.log(reverseArray(["A", "B", "C"]));
+// → ["C", "B", "A"];
+let arrayValue = [1, 2, 3, 4, 5];
+
+/* Resultat faux */ reverseArrayInPlace(arrayValue);
+console.log(arrayValue);
+// → [5, 4, 3, 2, 1]
